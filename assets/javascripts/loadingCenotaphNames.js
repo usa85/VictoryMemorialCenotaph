ca82ae6f34cf97ef75637f711d5a3286a97eba2c
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2019-10-23 11:46:18 -0500

    last change

--- assets/javascripts/loadingCenotaphNames.js
+++ assets/javascripts/loadingCenotaphNames.js
@@ -1,18 +1,17 @@
-
-
 document.addEventListener("DOMContentLoaded", ()=>{
 
-for (i = 0; i < cenotaph2.length; i++) {
-    let cenlist = document.querySelector("#list2");
-    //console.log(list);
-    //console.log(cenotaph2[i].name);
-    let element = document.createElement("li");
-    element.innerText = cenotaph2[i].name;
-    console.log(element);
+    for (i = 0; i < cenotaph2.length; i++) {
+        let cenlist = document.querySelector("#list2");
+        //console.log(list);
+        //console.log(cenotaph2[i].name);
+        let element = document.createElement("li");
+        element.innerText = cenotaph2[i].name;
+        console.log(element);
+        
     
-  
-    cenlist.appendChild(element);
-}});
+        cenlist.appendChild(element);
+    }
+});
 
 //bradleypauld@gmail.com
 //thursday 12:30
