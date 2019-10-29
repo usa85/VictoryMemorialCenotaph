16c66b1b889db8a84104206bc4be0d1ee648a5d3
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2019-10-29 16:36:14 -0500

    0.5rem

--- assets/javascripts/loadingCenotaphNames.js
+++ assets/javascripts/loadingCenotaphNames.js
@@ -4,7 +4,7 @@ basedescription = document.querySelector("#contents").innerHTML;
 function clickfunction(primaryID){
     console.log(primaryID);
     let content = document.querySelector("#contents");
-    content.innerHTML = '<h5>'+cenotaph2[primaryID].name + '</h5><p>' + cenotaph2[primaryID].rank + '<br>' + cenotaph2[primaryID].unit + '</p><p>Death Type: ' + cenotaph2[primaryID].deathType + '</p><p>Relative: ' + cenotaph2[primaryID].nearestRelative + '</p><p>Description: ' + cenotaph2[primaryID].description  + '</p><p>Journey: ' + cenotaph2[primaryID].journey + '<p>Cenotaph Picture: ' + cenotaph2[primaryID].imageFilename;
+    content.innerHTML = '<h5>'+cenotaph2[primaryID].name + '</h5><p>' + cenotaph2[primaryID].rank + '<br>' + cenotaph2[primaryID].unit + '</p><p>Death Type: ' + cenotaph2[primaryID].deathType + '</p><p>Relative: ' + cenotaph2[primaryID].nearestRelative + '</p><p>Description: ' + cenotaph2[primaryID].description  + '</p><p>Journey: ' + cenotaph2[primaryID].journey + '<p>Cenotaph Picture: ' + cenotaph2[primaryID].imageFilename +'</p><p>Battlefields: n/a</p><p>Unit Campgrounds: n/a</p><p>burrial sites: n/a </p><p>Last known address: n/a</p><p>Last known Resting Place: n/a</p><p>Family Input: n/a</p><p>';
     //'<br>Cenotaph: ' + cenotaph2[primaryID].imageFilename console.log(cenotaph2[primaryID].name);
 };
 
